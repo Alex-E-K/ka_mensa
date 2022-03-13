6b5c712ca038cab2418dfa11869bfc2cc0480222
Alexander Keller <a.e.keller@outlook.de>
2022-03-13 23:42:04 +0100

    Change default language to german

--- lib/data/constants/languages.dart
+++ lib/data/constants/languages.dart
@@ -2,6 +2,6 @@ import 'package:ka_mensa/data/constants/supported_locales.dart';
 import 'package:ka_mensa/data/model/language_model.dart';
 
 List<LanguageModel> languages = <LanguageModel>[
+  LanguageModel(supportedLocales[1], 'german'),
   LanguageModel(supportedLocales[0], 'english'),
-  LanguageModel(supportedLocales[1], 'german')
 ];
